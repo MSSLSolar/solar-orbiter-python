c846c9509075f47e06ed906c5a59de910a0c135c
David Stansby <dstansby@gmail.com>
2021-04-28 19:56:06 +0100

    Improve aia/eui example

--- examples/remote_sensing/eui_and_aia.py
+++ examples/remote_sensing/eui_and_aia.py
@@ -1,9 +1,20 @@
 """
-Carrington map reprojection
-===========================
+Combining EUI and AIA maps
+==========================
+How to combine EUI and AIA maps to improve coverage of the Sun at any one time.
+
+Since Solar Orbiter is not positioned at the Earth, it is possible to combine
+remote sensing images from Solar Orbiter and Earth-based assets
+(in this example, SDO/AIA) to improve the fraction of the Sun we can see at
+any one time.
+
+In this example we take an EUI 174A full sun image, and combine it with
+an SDO/AIA 171A image to create an almost-complete map of the Sun. This
+is done by reprojecting each map into a Carrington frame of reference,
+and adding them together.
 """
 ###############################################################################
-# Importing required modules.
+# First import the required modules.
 import aiapy.calibrate
 from astropy.coordinates import SkyCoord
 import astropy.units as u
@@ -12,6 +23,7 @@ import matplotlib.pyplot as plt
 from reproject import reproject_interp
 from reproject.mosaicking import reproject_and_coadd
 import sunpy_soar
+from sunpy.coordinates import HeliographicCarrington, HeliographicStonyhurst
 import sunpy.map
 import sunpy.sun.constants
 from sunpy.net import Fido
@@ -20,28 +32,31 @@ from sunpy.net.attrs import Instrument, Level, Time, Wavelength
 from sunpy_soar.attrs import Identifier
 
 ###############################################################################
-# Create search attributes.
+# We'll start by searching for an EUI image. This is done using the sunpy_soar
+# package. We define the search attributes, do the search, and print the
+# result.
 instrument = Instrument('EUI')
 time = Time('2021-02-01', '2021-02-02')
 level = Level(2)
 identifier = Identifier('EUI-FSI174-IMAGE')
 
-###############################################################################
-# Do search.
 result = Fido.search(instrument, time, level, identifier)
 print(result)
 
 ###############################################################################
-# Download first file.
+# The search returned a number of results. We'll just download the first one
+# here, print the local path that it is downloaded to, and load it into a
+# sunpy map.
 files = Fido.fetch(result[0, 0])
 print(files)
-
 eui_map = sunpy.map.Map(files[0])
 
-
 ###############################################################################
-# Get AIA map
+# Next we'll search for an AIA map. Because `sunpy.net.Fido` provides a single
+# interface for searching different data sources, the code is very similar to
+# the code used earlier to get an EUI map.
 instrument = Instrument('AIA')
+# The third argument here tells sunpy to find a single map closest to that date
 time = Time('2021-02-01', '2021-02-02', eui_map.date)
 wavelength = Wavelength(171*u.Angstrom)
 
@@ -50,7 +65,9 @@ print(result)
 
 files = Fido.fetch(result[0, 0])
 print(files)
-
+###############################################################################
+# Now we'll load the downloaded AIA file into a sunpy map, normalise the data
+# to the exposure time, apply an emperical correction factor
 aia_map = sunpy.map.Map(files[0])
 aia_map = aiapy.calibrate.normalize_exposure(aia_map)
 # This is an empirical correction factor to make the two maps have equal
@@ -59,7 +76,7 @@ aia_map = sunpy.map.Map(aia_map.data * 126 / 98, aia_map.meta)
 aia_map.meta['rsun_ref'] = sunpy.sun.constants.radius.to_value(u.m)
 
 ###############################################################################
-# Plot maps side by side
+# Plot the maps side by side.
 fig = plt.figure()
 ax = fig.add_subplot(121, projection=eui_map)
 eui_map.plot(axes=ax, norm=aia_map.plot_settings['norm'], vmin=0)
@@ -67,22 +84,27 @@ ax = fig.add_subplot(122, projection=aia_map)
 aia_map.plot(axes=ax, vmin=0)
 
 ###############################################################################
-# Cereate output header
+# Next we will reproject both maps on to a Carrington frame of reference.
+# To do this we start by creating a FITS world coordinat system (WCS) header
+# corresponding to the output coordinate frame.
 
-# This is set deliberately low to reduce memory consumption
+# This is set deliberately low, and (at the cost of memory and processing time)
+# can be increased to increase the output resolution
 shape_out = (360, 720)
+ref_coord_observer = HeliographicStonyhurst(0*u.deg, 0*u.deg, sunpy.sun.constants.radius,
+                                            obstime=eui_map.date)
+ref_coord = HeliographicCarrington(0*u.deg, 0*u.deg, sunpy.sun.constants.radius,
+                                   obstime=eui_map.date,
+                                   observer=ref_coord_observer)
 
-header = sunpy.map.make_fitswcs_header(shape_out,
-                                       SkyCoord(0, 0, unit=u.deg,
-                                                frame="heliographic_stonyhurst",
-                                                obstime=eui_map.date),
+header = sunpy.map.make_fitswcs_header(shape_out, ref_coord,
                                        scale=[180 / shape_out[0],
                                               360 / shape_out[1]] * u.deg / u.pix,
                                        projection_code="CAR")
 out_wcs = WCS(header)
 
 ###############################################################################
-# Reproject
+# Next we reproject and add together the two maps
 array, footprint = reproject_and_coadd([eui_map, aia_map], out_wcs, shape_out,
                                        reproject_function=reproject_interp)
 
@@ -90,7 +112,7 @@ outmap = sunpy.map.Map((array, header))
 outmap.plot_settings = aia_map.plot_settings
 
 ###############################################################################
-# Plot the reprojected map
+# Finally, we'll plot the reprojected map.
 fig = plt.figure()
 ax = fig.add_subplot(projection=outmap)
 outmap.plot(axes=ax)
